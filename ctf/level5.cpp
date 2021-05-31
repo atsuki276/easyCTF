2de79c2cdad02db123a8e72e444d21a76ce94f9b
atsuki276 <atsuki0727276@gmail.com>
2021-05-31 10:54:13 +0900

    carser question

---
+++ ctf/level5.cpp
@@ -0,0 +1,33 @@
+/**
+ * HLGXJ{xlmwmw_Geiwev_uyiwxmsr}
+ * ↑のシーザー暗号の解読問題
+ * 4文字次にずらしている。
+ * 
+ * 下記のコードは解答例
+ * 
+ * */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char flag[] = "HLGXJ{xlmwmw_Geiwev_uyiwxmsr}";
+
+    for (int i = 0; i < strlen(flag); i++)
+    {
+        //記号は無視
+        if (flag[i] == '{' || flag[i] == '_' || flag[i] == '}')
+        {
+            continue;
+        }
+
+        flag[i] -= 4;
+
+        cout << flag[i];
+        
+    }
+    
+}
