7f7883303e8ca486ca7c6857b79ca74d6f23af2e
atsuki276 <atsuki0727276@gmail.com>
2021-06-13 11:22:53 +0900

    pointarの練習

--- practice/list0811.cpp
+++ practice/list0811.cpp
@@ -12,6 +12,11 @@ int main()
 	}
 
 	sp++;
-	std::cout << *sp;
+	std::cout << *sp << std::endl;
+
+	std::string string = "testRiterall";
+	const char* pstring = &string[0];
 	
+	pstring += 2;
+	std::cout << *pstring << std::endl;
 }
